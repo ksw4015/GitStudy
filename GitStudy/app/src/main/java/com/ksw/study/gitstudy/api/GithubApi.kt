ad9c8f21a00a13e03f46d3f9a5e2f49ea87590c2
unknown <zealot4321@naver.com>
2020-05-14 14:53:59 +0900

    add dependency in gradle

---
+++ GitStudy/app/src/main/java/com/ksw/study/gitstudy/api/GithubApi.kt
@@ -0,0 +1,2 @@
+package com.ksw.study.gitstudy.api
+
---
+++ GitStudy/app/src/main/java/com/ksw/study/gitstudy/model/GithubRepo.kt
@@ -0,0 +1,8 @@
+package com.ksw.study.gitstudy.model
+
+data class GithubModel(
+    val name: String,
+    val desc: String,
+    val avatar: String,
+    val html: String
+)
